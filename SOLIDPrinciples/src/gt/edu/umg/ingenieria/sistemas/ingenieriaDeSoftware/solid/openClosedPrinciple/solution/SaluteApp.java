5e4715fc1faa7416b38a6cc2c46a4c96cbbc35bc
abarillas <abarillas@nbwaudev04>
2019-02-12 18:10:46 -0600

    Added static keyword to method in Translator class.

--- SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/ingenieriaDeSoftware/solid/openClosedPrinciple/solution/SaluteApp.java
+++ SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/ingenieriaDeSoftware/solid/openClosedPrinciple/solution/SaluteApp.java
@@ -2,14 +2,12 @@ package gt.edu.umg.ingenieria.sistemas.ingenieriaDeSoftware.solid.openClosedPrin
 
 public class SaluteApp {
     
-    public static void main(String[] args) {
-        Translator translator = new Translator();
-        
-        System.out.println("Hello in Spanish is said " + translator.salute(new SpanishSalute()));
-        System.out.println("Hello in English is said " + translator.salute(new EnglishSalute()));
-        System.out.println("Hello in French is said " + translator.salute(new FrenchSalute()));
-        System.out.println("Hello in Italian is said " + translator.salute(new ItalianSalute()));
-        System.out.println("Hello in German is said " + translator.salute(new GermanSalute()));
+    public static void main(String[] args) {        
+        System.out.println("Hello in Spanish is said " + Translator.salute(new SpanishSalute()));
+        System.out.println("Hello in English is said " + Translator.salute(new EnglishSalute()));
+        System.out.println("Hello in French is said " + Translator.salute(new FrenchSalute()));
+        System.out.println("Hello in Italian is said " + Translator.salute(new ItalianSalute()));
+        System.out.println("Hello in German is said " + Translator.salute(new GermanSalute()));
     }
     
 }
--- SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/ingenieriaDeSoftware/solid/openClosedPrinciple/solution/Translator.java
+++ SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/ingenieriaDeSoftware/solid/openClosedPrinciple/solution/Translator.java
@@ -2,7 +2,7 @@ package gt.edu.umg.ingenieria.sistemas.ingenieriaDeSoftware.solid.openClosedPrin
 
 public class Translator {
         
-    public String salute(Salute salute) {
+    public static String salute(Salute salute) {
         return salute.salute();
     }
     
