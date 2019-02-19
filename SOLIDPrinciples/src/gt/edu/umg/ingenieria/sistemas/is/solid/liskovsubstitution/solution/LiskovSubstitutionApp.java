48ec96dae1acbd203511ec0cb233e8e9f9462413
abarillas <abarillas@nbwaudev04>
2019-02-19 16:52:01 -0600

    Minor change on main method to use instance of parent class.

--- SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/liskovsubstitution/solution/LiskovSubstitutionApp.java
+++ SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/liskovsubstitution/solution/LiskovSubstitutionApp.java
@@ -3,6 +3,7 @@ package gt.edu.umg.ingenieria.sistemas.is.solid.liskovsubstitution.solution;
 public class LiskovSubstitutionApp {
 
     public static void main(String[] args) throws Exception {
+        ZooKeeper.makeAnimalMoveForward(new Animal());
         ZooKeeper.makeAnimalMoveForward(new Dog());
         ZooKeeper.makeAnimalMoveBackward(new Cat());        
         ZooKeeper.makeAnimalMoveForward(new Snake());
