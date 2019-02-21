0bbfca0743437459375124d9711ca8a0f0b26893
abarillas <abarillas@nbwaudev04>
2019-02-21 10:28:23 -0600

    Adding additional actions to Dog and Eagle classes.

--- SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/interfacesegregation/solution/Dog.java
+++ SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/interfacesegregation/solution/Dog.java
@@ -1,10 +1,15 @@
 package gt.edu.umg.ingenieria.sistemas.is.solid.interfacesegregation.solution;
 
-public class Dog extends Animal implements WalkAction {
+public class Dog extends Animal implements WalkAction, SwimAction {
         
     @Override
     public void walk() {
         System.out.println("I am a dog and I am walking!!!");        
+    }
+    
+    @Override
+    public void swim() {
+        System.out.println("I am a dog and I am swimming!!!");
     }
     
 }
--- SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/interfacesegregation/solution/Eagle.java
+++ SOLIDPrinciples/src/gt/edu/umg/ingenieria/sistemas/is/solid/interfacesegregation/solution/Eagle.java
@@ -1,10 +1,15 @@
 package gt.edu.umg.ingenieria.sistemas.is.solid.interfacesegregation.solution;
 
-public class Eagle extends Animal implements FlyAction {
+public class Eagle extends Animal implements FlyAction, WalkAction {
     
     @Override
     public void fly() {
         System.out.println("I am an eagle and I am flying!!!");        
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I am an eagle and I am walking!!!");
     }
     
 }
